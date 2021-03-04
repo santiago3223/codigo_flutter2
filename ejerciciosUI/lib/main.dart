f5ec58320f56d4f01b8ca3c0dcef9e97985dc655
santiago <santiago@finsera.com>
2021-03-03 19:48:25 -0500

    ejercicio4

--- ejerciciosUI/lib/main.dart
+++ ejerciciosUI/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'ejercicio3.dart';
-
 void main() {
   runApp(MyApp());
 }
@@ -12,7 +10,78 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: Ejercicio3(),
+      home: Ejercicio4(),
+    );
+  }
+}
+
+class Ejercicio4 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: IconButton(
+              icon: Icon(
+                Icons.category,
+                color: Colors.black,
+              ),
+              onPressed: () {}),
+          centerTitle: true,
+          title: Text(
+            "Categories",
+            style: TextStyle(color: Colors.black),
+          ),
+          actions: [
+            IconButton(
+                icon: Icon(
+                  Icons.search,
+                  color: Colors.black,
+                ),
+                onPressed: () {})
+          ],
+          bottom: TabBar(
+            labelColor: Colors.pink,
+            indicatorColor: Colors.pink,
+            unselectedLabelColor: Colors.black,
+            tabs: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("Para Ti"),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("Disenio"),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("Belleza"),
+              )
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Container(
+              child: Center(
+                child: Text("Tab1"),
+              ),
+            ),
+            Container(
+              child: Center(
+                child: Text("Tab1"),
+              ),
+            ),
+            Container(
+              child: Center(
+                child: Text("Tab1"),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
