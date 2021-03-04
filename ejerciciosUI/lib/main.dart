d83caaed7d023f3fddcc2135d0a8cdf8b76874ae
santiago <santiago@finsera.com>
2021-03-03 21:46:17 -0500

    ejercicio4

--- ejerciciosUI/lib/main.dart
+++ ejerciciosUI/lib/main.dart
@@ -65,8 +65,78 @@ class Ejercicio4 extends StatelessWidget {
         body: TabBarView(
           children: [
             Container(
-              child: Center(
-                child: Text("Tab1"),
+              color: Colors.grey.shade300,
+              child: Column(
+                children: [
+                  Container(
+                    color: Colors.white,
+                    margin: EdgeInsets.all(16),
+                    child: Stack(
+                      children: [
+                        Container(
+                          height: 100,
+                          width: 100,
+                          color: Colors.pink.shade100,
+                        ),
+                        Container(
+                            margin: EdgeInsets.all(16),
+                            color: Colors.white,
+                            child: Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(16.0),
+                                  child: Image.network(
+                                    "https://upload.wikimedia.org/wikipedia/commons/3/35/Elvis_Presley_Jailhouse_Rock.jpg",
+                                    width: 75,
+                                    height: 100,
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "How to seem like you always have your shot together, How to seem like you always have your shot together , How to seem like you always have your shot together",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 20),
+                                        ),
+                                        Row(
+                                          children: [
+                                            Icon(
+                                              Icons.circle,
+                                              color: Colors.pink,
+                                              size: 36,
+                                            ),
+                                            Text(
+                                              "Jonhy Vino",
+                                              style: TextStyle(fontSize: 16),
+                                            ),
+                                            SizedBox(
+                                              width: 20,
+                                            ),
+                                            Text(
+                                              "4 min",
+                                            ),
+                                          ],
+                                        ),
+                                        SizedBox(
+                                          height: 8,
+                                        ),
+                                        Text("read")
+                                      ],
+                                    ),
+                                  ),
+                                )
+                              ],
+                            )),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
             Container(
@@ -79,6 +149,20 @@ class Ejercicio4 extends StatelessWidget {
                 child: Text("Tab1"),
               ),
             )
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 1,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.pink,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: ""),
+            BottomNavigationBarItem(icon: Icon(Icons.folder), label: ""),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.favorite_border), label: ""),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_outline), label: ""),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: ""),
           ],
         ),
       ),
