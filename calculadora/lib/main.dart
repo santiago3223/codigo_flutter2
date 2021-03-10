8e7ee11c0bcbf488522583c39593e5e676bc271a
santiago <santiago@finsera.com>
2021-03-09 19:57:14 -0500

    calculadora

--- calculadora/lib/main.dart
+++ calculadora/lib/main.dart
@@ -25,10 +25,10 @@ class Calculadora extends StatefulWidget {
 
 class _CalculadoraState extends State<Calculadora> {
   List opcionesLinea1 = [];
-  List opcionesLinea2 = ["7", "8", "9", "x", "(", ")"];
-  List opcionesLinea3 = ["4", "5", "6", "-", "(", ")"];
-  List opcionesLinea4 = ["1", "2", "3", "+", "(", ")"];
-  List opcionesLinea5 = ["e", "0", ".", "=", "(", ")"];
+  List opcionesLinea2 = [];
+  List opcionesLinea3 = [];
+  List opcionesLinea4 = [];
+  List opcionesLinea5 = [];
   String display = "";
 
   @override
@@ -39,6 +39,38 @@ class _CalculadoraState extends State<Calculadora> {
       BotonCalculadora(etiqueta: "<-", funcion: borrarCaracter),
       BotonCalculadora(etiqueta: "%", funcion: agregarCaracter),
       BotonCalculadora(etiqueta: "/", funcion: agregarCaracter),
+      BotonCalculadora(etiqueta: "(", funcion: agregarCaracter),
+      BotonCalculadora(etiqueta: ")", funcion: agregarCaracter),
+    ];
+    opcionesLinea2 = opcionesLinea2 = [
+      BotonCalculadora(etiqueta: "7", funcion: agregarCaracter),
+      BotonCalculadora(etiqueta: "8", funcion: agregarCaracter),
+      BotonCalculadora(etiqueta: "9", funcion: agregarCaracter),
+      BotonCalculadora(etiqueta: "-", funcion: agregarCaracter),
+      BotonCalculadora(etiqueta: "(", funcion: agregarCaracter),
+      BotonCalculadora(etiqueta: ")", funcion: agregarCaracter),
+    ];
+    opcionesLinea3 = opcionesLinea3 = [
+      BotonCalculadora(etiqueta: "4", funcion: agregarCaracter),
+      BotonCalculadora(etiqueta: "5", funcion: agregarCaracter),
+      BotonCalculadora(etiqueta: "6", funcion: agregarCaracter),
+      BotonCalculadora(etiqueta: "-", funcion: agregarCaracter),
+      BotonCalculadora(etiqueta: "(", funcion: agregarCaracter),
+      BotonCalculadora(etiqueta: ")", funcion: agregarCaracter),
+    ];
+    opcionesLinea4 = opcionesLinea4 = [
+      BotonCalculadora(etiqueta: "1", funcion: agregarCaracter),
+      BotonCalculadora(etiqueta: "2", funcion: agregarCaracter),
+      BotonCalculadora(etiqueta: "3", funcion: agregarCaracter),
+      BotonCalculadora(etiqueta: "+", funcion: agregarCaracter),
+      BotonCalculadora(etiqueta: "(", funcion: agregarCaracter),
+      BotonCalculadora(etiqueta: ")", funcion: agregarCaracter),
+    ];
+    opcionesLinea5 = opcionesLinea5 = [
+      BotonCalculadora(etiqueta: "e", funcion: agregarCaracter),
+      BotonCalculadora(etiqueta: "0", funcion: agregarCaracter),
+      BotonCalculadora(etiqueta: ".", funcion: agregarCaracter),
+      BotonCalculadora(etiqueta: "=", funcion: agregarCaracter),
       BotonCalculadora(etiqueta: "(", funcion: agregarCaracter),
       BotonCalculadora(etiqueta: ")", funcion: agregarCaracter),
     ];
@@ -68,10 +100,10 @@ class _CalculadoraState extends State<Calculadora> {
                 ],
               )),
           obtenerLinea(context, opcionesLinea1),
-          // obtenerLinea(context, opcionesLinea2),
-          // obtenerLinea(context, opcionesLinea3),
-          // obtenerLinea(context, opcionesLinea4),
-          // obtenerLinea(context, opcionesLinea5),
+          obtenerLinea(context, opcionesLinea2),
+          obtenerLinea(context, opcionesLinea3),
+          obtenerLinea(context, opcionesLinea4),
+          obtenerLinea(context, opcionesLinea5),
         ],
       ),
     );
@@ -90,7 +122,7 @@ class _CalculadoraState extends State<Calculadora> {
             crossAxisAlignment: CrossAxisAlignment.stretch, children: botones));
   }
 
-  Widget obtenerBoton(context, BotonCalculadora  botonCalculadora) {
+  Widget obtenerBoton(context, BotonCalculadora botonCalculadora) {
     return Expanded(
         child: TextButton(
             onPressed: () {
