2d1840ff69ca41d826f7fd3a7bdd00203d293701
santiago <santiago@finsera.com>
2021-03-10 21:46:17 -0500

    tres en raya

--- tres_en_raya/lib/main.dart
+++ tres_en_raya/lib/main.dart
@@ -25,6 +25,7 @@ class TresEnRaya extends StatefulWidget {
 class _TresEnRayaState extends State<TresEnRaya> {
   List<String> botones = ["", "", "", "", "", "", "", "", ""];
   bool turno_jugador_1 = true;
+  bool multijugador = false;
   int ganador = 0;
 
   @override
@@ -34,6 +35,20 @@ class _TresEnRayaState extends State<TresEnRaya> {
       body: Container(
         child: Column(
           children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Un Jugador"),
+                Switch(
+                    value: multijugador,
+                    onChanged: (b) {
+                      setState(() {
+                        multijugador = b;
+                      });
+                    }),
+                Text("Multijugador")
+              ],
+            ),
             Text(
               "Numero de partida: 100",
               style: Theme.of(context).textTheme.headline5,
@@ -138,14 +153,26 @@ class _TresEnRayaState extends State<TresEnRaya> {
     if (botones[i] == "") {
       setState(() {
         botones[i] = turno_jugador_1 ? "X" : "O";
-        verificarGanador();
+        if (verificarGanador(botones)) {
+          setState(() {
+            ganador = turno_jugador_1 ? 1 : 2;
+          });
+        }
         turno_jugador_1 = !turno_jugador_1;
       });
-      
+      if (!multijugador && !turno_jugador_1) {
+        presionoBoton(obtenerMejorJugada());
+      }
     }
   }
 
-  void verificarGanador() {
+  int obtenerMejorJugada() {
+    List mm = minimax(botones, turno_jugador_1);
+
+    return mm[1];
+  }
+
+  bool verificarGanador(botones) {
     List pos_ganadoras = [
       [0, 1, 2],
       [3, 4, 5],
@@ -167,12 +194,36 @@ class _TresEnRayaState extends State<TresEnRaya> {
       }
 
       if (hay_ganador) {
-        setState(() {
-          ganador= turno_jugador_1?1:2;
-        });
-        return;
+        return true;
       }
     }
-    print("no hay ganador");
+    return false;
+  }
+
+  List minimax(board, player) {
+    if (verificarGanador(board)) {
+      return [1, 0];
+    }
+
+    int move = -1;
+    int score = -2;
+
+    for (var i = 0; i < board.length; ++i) {
+      if (board[i] == "") {
+        List boardWithNewMove = List.from(board);
+        boardWithNewMove[i] = player ? "X" : "O";
+        List mm = minimax(boardWithNewMove, !player);
+        int scoreForTheMove = -mm[0];
+        if (scoreForTheMove > score) {
+          score = scoreForTheMove;
+          move = i;
+        }
+      }
+
+    }
+    if (move == -1) {
+      return [0, -1];
+    }
+    return [score, move];
   }
 }
