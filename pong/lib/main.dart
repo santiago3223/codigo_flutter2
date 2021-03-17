1f5e964b70587ada6cde5823e6bb38ba95d64d9e
santiago <santiago@finsera.com>
2021-03-16 20:50:40 -0500

    pong

--- pong/lib/main.dart
+++ pong/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:pong/ball.dart';
 
 import 'bat.dart';
 
-enum Direction{up, down, left, right}
+enum Direction { up, down, left, right }
 
 void main() {
   runApp(MyApp());
@@ -31,11 +31,12 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
   double width;
   double heigth;
-  double posX =0;
-  double posY =0;
+  double posX = 0;
+  double posY = 0;
   double batWidth = 100;
   double batHeigth = 20;
   double batPosition = 0;
@@ -44,57 +45,86 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
   Direction vDir = Direction.down;
   Direction hDir = Direction.right;
 
-  void checkBorders(){
-    if(posX <= 0 && hDir==Direction.left){
+  void checkBorders() {
+    if (posX <= 0 && hDir == Direction.left) {
       hDir = Direction.right;
+      
     }
-    if(posX >= width && hDir==Direction.right){
+    if (posX >= width - 50 && hDir == Direction.right) {
       hDir = Direction.left;
     }
-    if(posY <= 0 && vDir==Direction.up){
+    if (posY <= 0 && vDir == Direction.up) {
       vDir = Direction.down;
     }
-    if(posY >= heigth && vDir==Direction.down){
+    if (posY >= heigth -125&&
+        vDir == Direction.down) {
       vDir = Direction.up;
     }
+
+    if (posY>=heigth-145){
+      if(posX >= batPosition-50 && posX <= (batPosition + batWidth+50)){
+        vDir = Direction.up;
+      }else{
+        controller.stop();
+      }
+    }
+
+    
   }
 
   @override
   void initState() {
-   
     posX = 0;
     posY = 0;
-    controller = AnimationController(duration: Duration(minutes: 10000), vsync: this);
-    animation = Tween<double>(begin: 0, end: 100).animate(controller);
-    animation.addListener(() { 
+    controller =
+        AnimationController(duration: Duration(minutes: 1000), vsync: this);
+    animation = Tween<double>(begin: 0, end: 1000).animate(controller);
+    animation.addListener(() {
       setState(() {
-        posX= hDir==Direction.right? posX+1:posX-1;
-        posY= vDir==Direction.down? posY+1:posY-1;
+
+        posX = hDir == Direction.right ? posX + 1 : posX - 1;
+        posY = vDir == Direction.down ? posY + 1 : posY - 1;
       });
       checkBorders();
     });
     controller.forward();
     super.initState();
-    
   }
 
   @override
   Widget build(BuildContext context) {
     width = MediaQuery.of(context).size.width;
     heigth = MediaQuery.of(context).size.height;
-    batWidth = width/5;
-    
+    batWidth = width / 5;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Container(
-        child: Stack(children: [
-          Positioned(child: Ball(),top :posY, left: posX,),
-          Positioned(child: Bat(width: batWidth, heigth: 20,),bottom :0),
-        ],),
-      )
-    );
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Container(
+          child: Stack(
+            children: [
+              Positioned(
+                child: Ball(),
+                top: posY,
+                left: posX,
+              ),
+              Positioned(
+                bottom: 0,
+                left: batPosition,
+                child: GestureDetector(
+                  onHorizontalDragUpdate: (DragUpdateDetails update){
+                    setState(() {
+                      batPosition += update.delta.dx;
+                    });
+                  } ,
+                    child: Bat(
+                  width: batWidth,
+                  heigth: 20,
+                )),
+              ),
+            ],
+          ),
+        ));
   }
 }
