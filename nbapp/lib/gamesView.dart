a03a84f3c4b74d000850ae25c93e7e24aff961c9
santiago <santiago@finsera.com>
2021-03-30 21:01:51 -0500

    nbapp

--- nbapp/lib/gamesView.dart
+++ nbapp/lib/gamesView.dart
@@ -27,34 +27,113 @@ class _GamesViewState extends State<GamesView> {
                         "https://i.pinimg.com/originals/e9/5f/56/e95f56d52fea3ab71221fdc5d10efbd0.jpg"),
                     fit: BoxFit.cover)),
           ),
-          FutureBuilder(
-            future:
-                http.get(Uri.parse("https://www.balldontlie.io/api/v1/games?seasons[]=2020&team_ids[]=${widget.equipo.id}")),
-            builder: (c, s) {
-              if (s.hasData) {
-                List<Game> games = jsonDecode(s.data.body)["data"]
-                    .map<Game>((m) => Game.fromJson(m))
-                    .toList();
-                games.sort((g1, g2) => g2.date.compareTo(g1.date));
-                return Container(
-                  child: ListView.builder(
-                      itemCount: games.length,
-                      itemBuilder: (c, i) => GameCard(game: games[i])),
-                );
-              }
-              if (s.hasError) {
-                return Center(
-                  child: Text(s.error.toString()),
-                );
-              }
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            },
+          Column(
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                widget.equipo.fullName,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  shadows: <Shadow>[
+                    Shadow(
+                      offset: Offset(3.0, 3.0),
+                      blurRadius: 3.0,
+                      color: Colors.black,
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: FutureBuilder(
+                  future: http.get(Uri.parse(
+                      "https://www.balldontlie.io/api/v1/games?seasons[]=2020&team_ids[]=${widget.equipo.id}")),
+                  builder: (c, s) {
+                    if (s.hasData) {
+                      List<Game> games = jsonDecode(s.data.body)["data"]
+                          .map<Game>((m) => Game.fromJson(m))
+                          .toList();
+                      int victorias = games
+                          .where((element) =>
+                              (element.visitorTeamScore >
+                                      element.homeTeamScore &&
+                                  widget.equipo.id == element.visitorTeam.id) ||
+                              (element.visitorTeamScore <
+                                      element.homeTeamScore &&
+                                  widget.equipo.id == element.homeTeam.id))
+                          .length;
+                      games.sort((g1, g2) => g2.date.compareTo(g1.date));
+                      return Container(
+                        child: Column(
+                          children: [
+                            Expanded(
+                              child: ListView.builder(
+                                  itemCount: games.length,
+                                  itemBuilder: (c, i) =>
+                                      GameCard(game: games[i])),
+                            ),
+                            buildResultados(
+                              victorias,
+                              games.length - victorias,
+                            )
+                          ],
+                        ),
+                      );
+                    }
+                    if (s.hasError) {
+                      return Center(
+                        child: Text(s.error.toString()),
+                      );
+                    }
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         ],
       ),
     );
+  }
+
+  Container buildResultados(int victorias, int derrotas) {
+    return Container(
+        padding: EdgeInsets.all(8),
+        color: Colors.white,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Text("Victorias", style: TextStyle(fontSize: 20)),
+                Text(
+                  victorias.toString(),
+                  style: TextStyle(
+                      fontSize: 36,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Text("Derrotas", style: TextStyle(fontSize: 20)),
+                Text(
+                  derrotas.toString(),
+                  style: TextStyle(
+                      fontSize: 36,
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ],
+        ));
   }
 }
 
@@ -107,8 +186,8 @@ class GameCard extends StatelessWidget {
                     style: TextStyle(
                         fontSize: 36,
                         color: game.homeTeamScore < game.visitorTeamScore
-                          ? Colors.blue
-                          : Colors.black54,
+                            ? Colors.blue
+                            : Colors.black54,
                         fontWeight: game.homeTeamScore < game.visitorTeamScore
                             ? FontWeight.bold
                             : FontWeight.normal)),
