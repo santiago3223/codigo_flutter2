7c81d2bd5ea976313084ac8e0876fa3198f538c2
santiago <santiago@finsera.com>
2021-03-30 19:08:00 -0500

    nbapp

--- nbapp/lib/gamesView.dart
+++ nbapp/lib/gamesView.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'models/game.dart';
 import 'models/team.dart';
 
 class GamesView extends StatefulWidget {
@@ -14,7 +17,76 @@ class _GamesViewState extends State<GamesView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.equipo.fullName),),
+      appBar: AppBar(
+        title: Text(widget.equipo.fullName),
+      ),
+      body: FutureBuilder(
+        future: http.get(Uri.parse("https://www.balldontlie.io/api/v1/games")),
+        builder: (c, s) {
+          if (s.hasData) {
+            List<Game> games = jsonDecode(s.data.body)["data"]
+                .map<Game>((m) => Game.fromJson(m))
+                .toList();
+
+            return Container(
+              child: ListView.builder(
+                  itemCount: games.length,
+                  itemBuilder: (c, i) => GameCard(game: games[i])),
+            );
+          }
+          if (s.hasError) {
+            return Center(
+              child: Text(s.error.toString()),
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class GameCard extends StatelessWidget {
+  const GameCard({
+    Key key,
+    @required this.game,
+  }) : super(key: key);
+
+  final Game game;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.only(top: 8, left: 32, right: 32),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+              child: Text(
+            game.homeTeam.name,
+            style: TextStyle(fontSize: 20),
+            textAlign: TextAlign.center,
+          )),
+          Text(
+            game.homeTeamScore.toString(),
+            style: TextStyle(fontSize: 36),
+          ),
+          Text(
+            "-",
+            style: TextStyle(fontSize: 36),
+          ),
+          Text(game.visitorTeamScore.toString(),
+              style: TextStyle(fontSize: 36)),
+          Expanded(
+              child: Text(
+            game.visitorTeam.name,
+            style: TextStyle(fontSize: 20),
+            textAlign: TextAlign.center,
+          )),
+        ],
+      ),
     );
   }
 }
