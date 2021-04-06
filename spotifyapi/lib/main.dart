2b1e0dc2103ce7250c21baf6b86ae0e04794ad7e
santiago <santiago@finsera.com>
2021-04-05 22:05:16 -0500

    spotifyapi

--- spotifyapi/lib/main.dart
+++ spotifyapi/lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:logger/logger.dart';
@@ -36,15 +35,19 @@ class _MyHomePageState extends State<MyHomePage> {
   Logger logger = Logger();
   BusquedaArtistas busquedaArtistas;
   TextEditingController controller = TextEditingController();
-  String apiUrl= "https://api.spotify.com/v1/";
+  List<Map> filtros = [
+    {"nombre": "album", "valor": false},
+    {"nombre": "artist", "valor": false},
+    {"nombre": "playlist", "valor": false},
+    {"nombre": "track", "valor": false},
+  ];
+  String apiUrl = "https://api.spotify.com/v1/";
 
   void buscarArtistas(String url) async {
-    http.Response response = await http.get(
-        Uri.parse(url),
-        headers: {
-          "Authorization":
-              "Bearer BQB7vZxC8cfDLVNWQQyUU2GUB7aCpwbMJkw3tEJD1VwWavw0OpI28prT5yfhfhpsAgfrWPQpnooRWlwD_aRfj51J79g_mkJDUpVSnbX9qpz627g56Qunt5cJuwFC2RKZ1L7tDqC8VPmvURXA4H-Y2aL8Z4yzOV1Uhd7SwDJqskSUIpWzgYZ1iW5bvhDlZztCbMlWxTsgiDW6LvHlZwSKXJUB81cJDCmCWVYpvQbx8WKwcP3Uuu8tHbc-xa1oVItIhJCq9ScKSv9J2Eam6rabxA"
-        });
+    http.Response response = await http.get(Uri.parse(url), headers: {
+      "Authorization":
+          "Bearer BQB7vZxC8cfDLVNWQQyUU2GUB7aCpwbMJkw3tEJD1VwWavw0OpI28prT5yfhfhpsAgfrWPQpnooRWlwD_aRfj51J79g_mkJDUpVSnbX9qpz627g56Qunt5cJuwFC2RKZ1L7tDqC8VPmvURXA4H-Y2aL8Z4yzOV1Uhd7SwDJqskSUIpWzgYZ1iW5bvhDlZztCbMlWxTsgiDW6LvHlZwSKXJUB81cJDCmCWVYpvQbx8WKwcP3Uuu8tHbc-xa1oVItIhJCq9ScKSv9J2Eam6rabxA"
+    });
     if (response.statusCode == 200) {
       BusquedaArtistas bArtistas =
           BusquedaArtistas.fromJson(jsonDecode(response.body));
@@ -66,6 +69,47 @@ class _MyHomePageState extends State<MyHomePage> {
         children: [
           TextField(
             controller: controller,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ChoiceChip(
+                label: Text("Album"),
+                selected: filtros[0]["valor"],
+                onSelected: (b) {
+                  setState(() {
+                    filtros[0]["valor"] = b;
+                  });
+                },
+              ),
+              ChoiceChip(
+                label: Text("Artista"),
+                selected: filtros[1]["valor"],
+                onSelected: (b) {
+                  setState(() {
+                    filtros[0]["valor"] = b;
+                  });
+                },
+              ),
+              ChoiceChip(
+                label: Text("Playlist"),
+                selected: filtros[2]["valor"],
+                onSelected: (b) {
+                  setState(() {
+                    filtros[2]["valor"] = b;
+                  });
+                },
+              ),
+              ChoiceChip(
+                label: Text("Cancion"),
+                selected: filtros[3]["valor"],
+                onSelected: (b) {
+                  setState(() {
+                    filtros[3]["valor"] = b;
+                  });
+                },
+              ),
+            ],
           ),
           Expanded(
             child: Container(
@@ -96,18 +140,23 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           Row(
             children: [
-             busquedaArtistas.artists.previous==null?Container(): ElevatedButton.icon(
-                  onPressed: () {
-                    buscarArtistas(busquedaArtistas.artists.previous);
-                  },
-                  icon: Icon(Icons.arrow_back),
-                  label: Text("Atras")),
-              busquedaArtistas.artists.next==null?Container(): ElevatedButton.icon(
-                  onPressed: () {
-                    buscarArtistas(busquedaArtistas.artists.next);
-                  },
-                  icon: Icon(Icons.arrow_forward),
-                  label: Text("Siguiente"))
+              busquedaArtistas == null ||
+                      busquedaArtistas.artists.previous == null
+                  ? Container()
+                  : ElevatedButton.icon(
+                      onPressed: () {
+                        buscarArtistas(busquedaArtistas.artists.previous);
+                      },
+                      icon: Icon(Icons.arrow_back),
+                      label: Text("Atras")),
+              busquedaArtistas == null || busquedaArtistas.artists.next == null
+                  ? Container()
+                  : ElevatedButton.icon(
+                      onPressed: () {
+                        buscarArtistas(busquedaArtistas.artists.next);
+                      },
+                      icon: Icon(Icons.arrow_forward),
+                      label: Text("Siguiente"))
             ],
           )
         ],
@@ -115,7 +164,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.search),
         onPressed: () {
-          buscarArtistas(apiUrl+"search?q=${controller.text}&type=artist");
+          buscarArtistas(apiUrl + "search?q=${controller.text}&type=artist");
         },
       ),
     );
