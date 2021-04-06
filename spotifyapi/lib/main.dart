3a634e4a5227a673e645c5028623078264eff93e
santiago <santiago@finsera.com>
2021-04-05 19:52:48 -0500

    spotifyapi

--- spotifyapi/lib/main.dart
+++ spotifyapi/lib/main.dart
@@ -3,7 +3,9 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:logger/logger.dart';
+import 'package:logger_flutter/logger_flutter.dart';
 import 'package:spotifyapi/moderls/busqueda_artistas.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -34,40 +36,38 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   Logger logger = Logger();
+  BusquedaArtistas busquedaArtistas;
 
   void buscarArtistas() async {
     http.Response response =
-        await http.get(Uri.parse("https://api.spotify.com/v1/search"));
+        await http.get(Uri.parse("https://api.spotify.com/v1/search?q=metallica&type=artist"),headers: {
+          "Authorization" : "Bearer BQDW9LPYvUc6AUV39Tgb68KLtgWeOWZbZFgI1TYyhommiX6kPGJE9g93-2TMyclVd7c2BeNP16X2yjhbU-8AMVNhvoQDcLX394zIu78jyCLqzmnzPOiY7pIJv2t-OCTRuXbO9IwP1KhZIcUDFs043eLjReRsOhb2w6vor7LJlsCwwdESkhyWnU_U08QvjPiJflpwPsl2JbgboYdeojPhu4u7MNHeEmyquh9uTAu2H7_P4ILuuJP3MtgEMy-2xfIg0QHthQRbtTm6-A1tzpdU4A"
+        });
     if (response.statusCode == 200) {
       BusquedaArtistas bArtistas =
           BusquedaArtistas.fromJson(jsonDecode(response.body));
-      print(bArtistas);
+      setState(() {
+        busquedaArtistas = bArtistas;
+      });
     } else {
-      logger.e(response.body.toString());
+      logger.e(jsonDecode(response.body).toString());
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    buscarArtistas();
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Container(
+        child: ListView.builder(
+          itemCount: busquedaArtistas.artists.items.length,
+          itemBuilder: (c,i)=>ListTile(title: Text(busquedaArtistas.artists.items[i].name),)),
       ),
+      floatingActionButton: FloatingActionButton(child: Icon(Icons.search), onPressed: (){
+        buscarArtistas();
+      },),
     );
   }
 }
