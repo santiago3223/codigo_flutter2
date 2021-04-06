7a09d04bb8627c83ab60cab3a9f96a41ff830203
santiago <santiago@finsera.com>
2021-04-05 19:18:01 -0500

    spotifyapi

---
+++ spotifyapi/lib/main.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
+import 'package:spotifyapi/moderls/busqueda_artistas.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Logger logger = Logger();
+
+  void buscarArtistas() async {
+    http.Response response =
+        await http.get(Uri.parse("https://api.spotify.com/v1/search"));
+    if (response.statusCode == 200) {
+      BusquedaArtistas bArtistas =
+          BusquedaArtistas.fromJson(jsonDecode(response.body));
+      print(bArtistas);
+    } else {
+      logger.e(response.body.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    buscarArtistas();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ spotifyapi/lib/moderls/busqueda_artistas.dart
@@ -0,0 +1,197 @@
+// To parse this JSON data, do
+//
+//     final busquedaArtistas = busquedaArtistasFromJson(jsonString);
+
+import 'dart:convert';
+
+BusquedaArtistas busquedaArtistasFromJson(String str) => BusquedaArtistas.fromJson(json.decode(str));
+
+String busquedaArtistasToJson(BusquedaArtistas data) => json.encode(data.toJson());
+
+class BusquedaArtistas {
+    BusquedaArtistas({
+        this.artists,
+    });
+
+    Artists artists;
+
+    factory BusquedaArtistas.fromJson(Map<String, dynamic> json) => BusquedaArtistas(
+        artists: Artists.fromJson(json["artists"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "artists": artists.toJson(),
+    };
+}
+
+class Artists {
+    Artists({
+        this.href,
+        this.items,
+        this.limit,
+        this.next,
+        this.offset,
+        this.previous,
+        this.total,
+    });
+
+    String href;
+    List<Item> items;
+    int limit;
+    String next;
+    int offset;
+    dynamic previous;
+    int total;
+
+    factory Artists.fromJson(Map<String, dynamic> json) => Artists(
+        href: json["href"],
+        items: List<Item>.from(json["items"].map((x) => Item.fromJson(x))),
+        limit: json["limit"],
+        next: json["next"],
+        offset: json["offset"],
+        previous: json["previous"],
+        total: json["total"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "href": href,
+        "items": List<dynamic>.from(items.map((x) => x.toJson())),
+        "limit": limit,
+        "next": next,
+        "offset": offset,
+        "previous": previous,
+        "total": total,
+    };
+}
+
+class Item {
+    Item({
+        this.externalUrls,
+        this.followers,
+        this.genres,
+        this.href,
+        this.id,
+        this.images,
+        this.name,
+        this.popularity,
+        this.type,
+        this.uri,
+    });
+
+    ExternalUrls externalUrls;
+    Followers followers;
+    List<String> genres;
+    String href;
+    String id;
+    List<Image> images;
+    String name;
+    int popularity;
+    Type type;
+    String uri;
+
+    factory Item.fromJson(Map<String, dynamic> json) => Item(
+        externalUrls: ExternalUrls.fromJson(json["external_urls"]),
+        followers: Followers.fromJson(json["followers"]),
+        genres: List<String>.from(json["genres"].map((x) => x)),
+        href: json["href"],
+        id: json["id"],
+        images: List<Image>.from(json["images"].map((x) => Image.fromJson(x))),
+        name: json["name"],
+        popularity: json["popularity"],
+        type: typeValues.map[json["type"]],
+        uri: json["uri"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "external_urls": externalUrls.toJson(),
+        "followers": followers.toJson(),
+        "genres": List<dynamic>.from(genres.map((x) => x)),
+        "href": href,
+        "id": id,
+        "images": List<dynamic>.from(images.map((x) => x.toJson())),
+        "name": name,
+        "popularity": popularity,
+        "type": typeValues.reverse[type],
+        "uri": uri,
+    };
+}
+
+class ExternalUrls {
+    ExternalUrls({
+        this.spotify,
+    });
+
+    String spotify;
+
+    factory ExternalUrls.fromJson(Map<String, dynamic> json) => ExternalUrls(
+        spotify: json["spotify"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "spotify": spotify,
+    };
+}
+
+class Followers {
+    Followers({
+        this.href,
+        this.total,
+    });
+
+    dynamic href;
+    int total;
+
+    factory Followers.fromJson(Map<String, dynamic> json) => Followers(
+        href: json["href"],
+        total: json["total"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "href": href,
+        "total": total,
+    };
+}
+
+class Image {
+    Image({
+        this.height,
+        this.url,
+        this.width,
+    });
+
+    int height;
+    String url;
+    int width;
+
+    factory Image.fromJson(Map<String, dynamic> json) => Image(
+        height: json["height"],
+        url: json["url"],
+        width: json["width"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "height": height,
+        "url": url,
+        "width": width,
+    };
+}
+
+enum Type { ARTIST }
+
+final typeValues = EnumValues({
+    "artist": Type.ARTIST
+});
+
+class EnumValues<T> {
+    Map<String, T> map;
+    Map<T, String> reverseMap;
+
+    EnumValues(this.map);
+
+    Map<T, String> get reverse {
+        if (reverseMap == null) {
+            reverseMap = map.map((k, v) => new MapEntry(v, k));
+        }
+        return reverseMap;
+    }
+}
