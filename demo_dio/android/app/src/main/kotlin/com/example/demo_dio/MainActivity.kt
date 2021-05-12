9c5df1b8fa08eebf6678ab400699d3dad609675d
santiago <santiago@finsera.com>
2021-05-12 18:33:57 -0500

    dio

---
+++ demo_dio/android/app/src/main/kotlin/com/example/demo_dio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.demo_dio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ demo_dio/lib/example.g.dart
@@ -0,0 +1,37 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'example.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+class _RestClient implements RestClient {
+  _RestClient(this._dio, {this.baseUrl}) {
+    ArgumentError.checkNotNull(_dio, '_dio');
+    baseUrl ??= 'https://5d42a6e2bc64f90014a56ca0.mockapi.io/api/v1/';
+  }
+
+  final Dio _dio;
+
+  String baseUrl;
+
+  @override
+  Future<List<Task>> getTasks() async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final _result = await _dio.request<List<dynamic>>('/tasks',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'GET',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    var value = _result.data
+        .map((dynamic i) => Task.fromJson(i as Map<String, dynamic>))
+        .toList();
+    return value;
+  }
+}
